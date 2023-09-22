acc927050ef412eb772bfe0011f8d1ec9c402ea3
zachariah-zb <112483894+zachariah-zb@users.noreply.github.com>
2023-09-22 15:09:20 +1000

    Update streamlit_app.py
    Changed header text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
